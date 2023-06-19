ecccf0e52548174c499a4436c3a3c514e266a515
Ye Lwin Oo <yelwinoo@ucsm.edu.mm>
2023-06-19 18:28:49 +0630

    course 3 added

---
+++ odyssey-lift-off-part3/server/src/schema.js
@@ -0,0 +1,50 @@
+const gql = require('graphql-tag');
+
+const typeDefs = gql`
+  type Query {
+    "Query to get tracks array for the homepage grid"
+    tracksForHome: [Track!]!
+    track(id: ID!): Track
+  }
+
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    "The track's title"
+    title: String!
+    "The track's main Author"
+    author: Author!
+    "The track's illustration to display in track card or track page detail"
+    thumbnail: String
+    "The track's approximate length to complete, in minutes"
+    length: Int
+    "The number of modules this track contains"
+    modulesCount: Int
+    "The description of the track"
+    description: String
+    "The number of views that people watch "
+    numberOfViews: Int
+    "An array of modules for the Track"
+    modules: [Module!]!
+  }
+
+  "A module is a single unit of teaching. multiple modules compose of a Track"
+  type Module{
+    id: ID!
+    "A module's title"
+    title: String!
+    "A module's length in minutes"
+    length: Int
+  }
+
+  "Author of a complete Track or a Module"
+  type Author {
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author's profile picture"
+    photo: String
+  }
+`;
+
+module.exports = typeDefs;
